50819fde5a61c0b07ff0c67fc1762e0a3323b3c4
Or Hasson <hassonor@gmail.com>
2024-08-14 20:10:36 +0300

    add HomeProperties page (last recent properties)

---
+++ components/HomeProperties.tsx
@@ -0,0 +1,34 @@
+import properties from '@/properties.json';
+import PropertyCard from './PropertyCard';
+import Link from "next/link";
+
+
+const HomeProperties = () => {
+    const recentProperties = properties.slice(0, 3);
+    return (<>
+        <section className='px-4 py-6'>
+            <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+                Recent Properties
+            </h2>
+            <div className='container-xl lg:container m-auto px-4 py-6'>
+                {properties.length === 0 ? (<p>No properties found</p>) : (
+                    <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                        {
+                            recentProperties.map((property, index) => (
+                                <PropertyCard key={property._id} property={property}/>
+                            ))
+                        }
+                    </div>
+                )}
+            </div>
+        </section>
+        <section className='m-auto max-w-lg my-6 px-6'>
+            <Link href='/properties'
+                  className='block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700'>
+                View All Properties
+            </Link>
+        </section>
+    </>);
+}
+
+export default HomeProperties;
