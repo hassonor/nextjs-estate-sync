9e1146905a6a2fb3e7338cece5e58dad7c43666d
Or Hasson <hassonor@gmail.com>
2024-08-16 17:50:01 +0300

    add propery add form

--- app/properties/add/page.tsx
+++ app/properties/add/page.tsx
@@ -1,5 +1,13 @@
+import PropertyAddForm from "@/components/PropertyAddForm";
+
 const AddPropertyPage = async () => {
-    return (<div>AddProperty Page</div>)
+    return (<section className="bg-blue-50">
+        <div className="container m-auto max-w-2xl py-20">
+            <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+                <PropertyAddForm/>
+            </div>
+        </div>
+    </section>)
 }
 
 export default AddPropertyPage;
