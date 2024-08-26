b7356dcd2ed290870af7e7d0aa9d3b853c1ca296
Or Hasson <hassonor@gmail.com>
2024-08-26 18:49:59 +0300

    add link to image

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -20,14 +20,18 @@ const PropertyCard = ({property}: PropertyCardProps) => {
     }
     return (
         <div className="rounded-xl shadow-md relative">
-            <Image
-                src={property.images[0]}
-                alt={property.name}
-                width='0'
-                height='0'
-                sizes='100vw'
-                className="w-full h-auto rounded-t-xl"
-            />
+            <Link
+                href={`/properties/${property._id}`}
+                className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm">
+                <Image
+                    src={property.images[0]}
+                    alt={property.name}
+                    width='0'
+                    height='0'
+                    sizes='100vw'
+                    className="w-full h-auto rounded-t-xl"
+                />
+            </Link>
             <div className="p-4">
                 <div className="text-left md:text-center lg:text-left mb-6">
                     <div className="text-gray-600">{property.type}</div>
